bd3de09062602108a4f230bc48600a3f6b70e70c
Abdualiym <a.abdualiym@gmail.com>
2020-02-09 01:02:57 +0500

    fix widget parenting

--- widgets/menu/views/_template.php
+++ widgets/menu/views/_template.php
@@ -1,34 +1,32 @@
 <?php
 
-use abdualiym\cms\entities\Menu;
 use abdualiym\cms\helpers\Language;
 
 $url = Yii::$app->language;
 
-foreach ($menu as $key => $value) {
-    if ($value['type'] != Menu::TYPE_EMPTY) {
-        echo '<li><a href="' . $value['link'] . '">' . Language::getAttribute($value, 'title', $url) . '</a></li>';
-    } else {
-        if (isset($value['childs']) && $value['childs']) {
-            echo '<li class="dropdown">' . '<a href="' . $value['link'] . '" class="dropdown-toggle" data-toggle="dropdown">' . Language::getAttribute($value, 'title', $url) . ' <b class="caret"></b></a>';
-            echo '<ul class="dropdown-menu">';
-            foreach ($value['childs'] as $key => $childValue) {
-                if (isset($childValue['childs']) && $childValue['childs']) {
-                    echo '<li class="dropdown">' . '<a href="' . $childValue['link'] . '" class="dropdown-toggle" data-toggle="dropdown">' . Language::getAttribute($childValue, 'title', $url) . ' <b class="caret"></b></a>';
-                    echo '<ul class="dropdown-menu">';
-                    foreach ($childValue['childs'] as $key => $children) {
-                        echo '<li><a href="' . $children['link'] . '">' . Language::getAttribute($children, 'title', $url) . '</a></li>';
-                    }
-                    echo '</ul>';
-                    echo '</li>';
-                } else {
-                    echo '<li><a href="' . $childValue['link'] . '">' . Language::getAttribute($childValue, 'title', $url) . '</a></li>';
-                }
-            }
-            echo '</ul>';
-            echo '</li>';
-        } else {
-            echo '<li><a href="' . $value['link'] . '">' . Language::getAttribute($value, 'title', $url) . '</a></li>';
-        }
-    }
-}
+foreach ($menu as $key => $value) : ?>
+    <?php $parent = isset($value['childs']) && $value['childs']; ?>
+    <li <?= $parent ? 'class="dropdown"' : '' ?>>
+        <a href="<?= $value['link'] ?>" <?= $parent ? 'class="dropdown-toggle" data-toggle="dropdown"' : '' ?>>
+            <?= Language::getAttribute($value, 'title', $url) ?> <?= $parent ? ' <b class="caret"></b>' : '' ?>
+        </a>
+        <?php if ($parent): ?>
+            <ul class="dropdown-menu">
+                <?php foreach ($value['childs'] as $key => $childValue) : ?>
+                    <?php if (isset($childValue['childs']) && $childValue['childs']) : ?>
+                        <li class="dropdown"><a href="<?= $childValue['link'] ?>" class="dropdown-toggle" data-toggle="dropdown"><?= Language::getAttribute($childValue, 'title', $url) ?> <b
+                                        class="caret"></b></a>
+                            <ul class="dropdown-menu">
+                                <?php foreach ($childValue['childs'] as $key => $children) : ?>
+                                    <li><a href="<?= $children['link'] ?>"><?= Language::getAttribute($children, 'title', $url) ?></a></li>
+                                <?php endforeach; ?>
+                            </ul>
+                        </li>
+                    <?php else: ?>
+                        <li><a href="<?= $childValue['link'] ?>"><?= Language::getAttribute($childValue, 'title', $url) ?></a></li>
+                    <?php endif; ?>
+                <?php endforeach; ?>
+            </ul>
+        <?php endif; ?>
+    </li>
+<?php endforeach; ?>
